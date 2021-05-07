6bc57790062c35b46909bef62afae538dfc26917
LucianoAlbanes <lucianoalbanes@gmail.com>
2021-05-07 16:26:27 -0300

    Initialized module

---
+++ lib/mymath.py
@@ -0,0 +1,5 @@
+def absoulte(n):
+    if n<0:
+        return n*-1
+    else:
+        return n
---
+++ personal_library.py
@@ -0,0 +1,27 @@
+from lib import mymath
+import sys
+
+# https://docs.python.org/3/tutorial/modules.html#executing-modules-as-scripts ''executing-modules-as-scripts''
+
+# Verify arguments from console to execute corresponding task
+if len(sys.argv) == 3:
+    if sys.argv[1] == '-create':
+        # Execute create fn
+        create()
+    elif sys.argv[1] == '-search':
+        # Execute search fn
+        search()
+    else:
+        print(f'Invalid argument.\nUse "-create <local_path>" or "-search <key_word>"')
+else:
+    print(f'Invalid argument.\nUse "-create <local_path>" or "-search <key_word>"')
+
+
+# Define functions
+
+def create():
+    return True
+
+
+def search():
+    return True
