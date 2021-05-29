e9d679cdfcb9ce2ec168c4ba269907a5e7a42dd9
LucianoAlbanes <lucianoalbanes@gmail.com>
2021-05-29 02:48:41 -0300

    Agregar codigo test e Info

--- personal_library.py
+++ personal_library.py
@@ -25,8 +25,27 @@ def search():
     return True
 
 # Test
+## INFO: Implementé la estructura, usando trie.insert(Trie, cadena) se insertará en el trie (con hash tables y todo eso).
+# Esta funcion retorna el puntero del nodo correspondiente a la ultima letra (En este caso 'A'),
+# de ahi tenemos acceso a el atributo docsWhereApears.
+
+
 from lib import trie
 T = trie.Trie
-trie.insert(T,'hola')
-for i in range(1123):
-    print('hi')
+nodoDePalabraInsertada = trie.insert(T, 'Hola')
+
+print(nodoDePalabraInsertada)
+print(nodoDePalabraInsertada.key) # Estoy guardando como key el resultado de lib.latinHash(), ahi mismo dice como lo calcula
+print(nodoDePalabraInsertada.docsWhereApears)
+print(nodoDePalabraInsertada.children)
+
+# Test fuerza bruta
+for i in range(100_000):
+    trie.insert(T, str(i))
+
+# Estoy pensando en crear una estructura que simplifique la cantidad de elementos, ya que por cada nodo se hace este camino:
+# trie.trieNode.children.array[i].linkedlist.dictionaryNode.trieNode
+# Capaz simplificarlo en una sola clase que cumpla de linkedlist, dictionaryNode y trieNode
+# Sea como sea, esto lo estoy implementando en los proximos dias. De todas maneras, la funcion trie.insert siempre les va a devolver
+# el pointer de la ultima palabra, que es lo que se necesita para modificar el docsWhereApears.
+# Cualquier cosa me dicen
